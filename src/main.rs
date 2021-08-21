9378509275e09cb113e82f9b1c8f3177c917cd11
Burcak Kam <budemirc@microsoft.com>
2021-08-21 14:06:28 +0300

    Draw the ball

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use ggez::{
     self, event,
-    graphics::{self, DrawMode, FillOptions},
+    graphics::{self, DrawMode},
     nalgebra as na, Context, GameResult,
 };
 
@@ -8,10 +8,15 @@ const RACKET_HEIGHT: f32 = 100.0;
 const RACKET_WIDTH: f32 = 20.0;
 const RACKET_HEIGHT_HALF: f32 = RACKET_HEIGHT * 0.5;
 const RACKET_WIDTH_HALF: f32 = RACKET_WIDTH * 0.5;
+const BALL_SIZE: f32 = 30.0;
+const BALL_STROKE: f32 = 3.5;
+const BALL_SIZE_HALF: f32 = BALL_SIZE * 0.5;
+const BALL_TOLERANCE: f32 = 0.1;
 
 struct MainState {
     player_1_pos: na::Point2<f32>,
     player_2_pos: na::Point2<f32>,
+    ball_pos: na::Point2<f32>,
 }
 
 impl MainState {
@@ -22,6 +27,7 @@ impl MainState {
         MainState {
             player_1_pos: na::Point2::new(RACKET_WIDTH_HALF, screen_h_half),
             player_2_pos: na::Point2::new(screen_w - RACKET_WIDTH_HALF, screen_h_half),
+            ball_pos: na::Point2::new(screen_w_half, screen_h_half),
         }
     }
 }
@@ -33,6 +39,7 @@ impl event::EventHandler for MainState {
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, graphics::BLACK);
 
+        // Draw rackets
         let racket_rect = graphics::Rect::new(
             -RACKET_WIDTH_HALF,
             -RACKET_HEIGHT_HALF,
@@ -46,13 +53,24 @@ impl event::EventHandler for MainState {
             graphics::WHITE,
         )?;
 
-        let mut draw_param_1 = graphics::DrawParam::default();
-        draw_param_1.dest = self.player_1_pos.into();
-        graphics::draw(ctx, &racket_mesh, draw_param_1)?;
+        let mut draw_param = graphics::DrawParam::default();
+        draw_param.dest = self.player_1_pos.into();
+        graphics::draw(ctx, &racket_mesh, draw_param)?;
 
-        let mut draw_param_2 = graphics::DrawParam::default();
-        draw_param_2.dest = self.player_2_pos.into();
-        graphics::draw(ctx, &racket_mesh, draw_param_2)?;
+        draw_param.dest = self.player_2_pos.into();
+        graphics::draw(ctx, &racket_mesh, draw_param)?;
+
+        // Draw ball
+        let ball_mesh = graphics::Mesh::new_circle::<na::Point2<f32>>(
+            ctx,
+            DrawMode::stroke(BALL_STROKE),
+            self.ball_pos.into(),
+            BALL_SIZE_HALF,
+            BALL_TOLERANCE,
+            graphics::WHITE,
+        )?;
+
+        graphics::draw(ctx, &ball_mesh, graphics::DrawParam::default())?;
 
         graphics::present(ctx)?;
         Ok(())

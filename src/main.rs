e083da0efa79eaaa5755fba7b743af9f6673ae59
Burcak Kam <budemirc@microsoft.com>
2021-08-21 13:26:07 +0300

    Created the game window and added event_loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use ggez::{self, event, graphics, Context, GameResult};
+
+struct MainState {}
+
+impl MainState {
+    pub fn new() -> Self {
+        MainState {}
+    }
+}
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+    fn draw(&mut self, _ctx: &mut Context) -> GameResult {
+        graphics::clear(_ctx, graphics::BLACK);
+        graphics::present(_ctx)?;
+        Ok(())
+    }
+}
+
+fn main() -> GameResult {
+    let cb = ggez::ContextBuilder::new("Pong", "BurcakKam");
+    let (ctx, event_loop) = &mut cb.build()?;
+    graphics::set_window_title(ctx, "Pong");
+
+    let mut state = MainState::new();
+    event::run(ctx, event_loop, &mut state)?;
+    Ok(())
 }
